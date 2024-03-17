ed3caac648cd2f27f7385e6e192269587e5aae23
Md Mohin <157195584+mohinal@users.noreply.github.com>
2024-03-17 16:45:10 +0600

    Add files via upload
    Real State project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop:true,
+    autoplay:{
+        delay:2000,
+    }
+  });
